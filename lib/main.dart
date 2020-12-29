36cf01842a575b6143be509a2d1dea0304303ecc
rinonkia <git@gatapon.net>
2020-12-29 15:20:39 +0900

    create GetUserName

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 // Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
 import 'package:learn_ddd/add_user.dart';
+import 'package:learn_ddd/get_user_name.dart';
 
 void main() {
   // https://stackoverflow.com/questions/57689492/flutter-unhandled-exception-servicesbinding-defaultbinarymessenger-was-accesse
@@ -140,6 +141,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headline4,
             ),
             AddUser('Akinori', 'Diverse', 32),
+            GetUserName('uGCwAm5nUaGIIxmEKmxF')
           ],
         ),
       ),
